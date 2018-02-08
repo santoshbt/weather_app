bec909c8f46eed66d6728cd43edbf5b72a3b0b0a
Santosh Turamari <santoshturamari@Santoshs-MacBook-Pro.local>
2018-02-08 15:12:42 +0530

    first commit

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from  '../actions/index';
+
+export default function(state= [], action){
+  switch(action.type){
+    case FETCH_WEATHER:
+      // return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ]; // compact way of creating new array
+  }
+
+  return state;
+}
